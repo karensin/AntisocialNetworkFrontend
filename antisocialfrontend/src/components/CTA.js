6cccc6a0a64d9b60b864d3bba7a9f2307610bc44
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-01-26 15:15:40 -0800

    pop up added

--- antisocialfrontend/src/components/CTA.js
+++ antisocialfrontend/src/components/CTA.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Modal } from 'react-bootstrap'
-// import SignUp from './SignUp.js';
+import SignUp from './SignUp.js';
 import { Form, Button, Col } from 'react-bootstrap/';
 
 class CTA extends Component {
@@ -27,34 +27,8 @@ class CTA extends Component {
                 <Modal show={this.state.show} onHide={() => this.handleModal()}>
                     <Modal.Header closeButton > Head Part</Modal.Header>
                     <Modal.Body>
-                        <Form className="SignUp">
-                            <Form.Row>
-                                <Col>
-                                    <Form.Label>First Name</Form.Label>
 
-                                    <Form.Control placeholder="First name" />
-                                </Col>
-                                <Col>
-                                    <Form.Label>Last Name</Form.Label>
-
-                                    <Form.Control placeholder="Last name" />
-                                </Col>
-                            </Form.Row>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-              </Form.Text>
-                            </Form.Group>
-                            <Form.Check
-                                required
-                                label="Agree to terms and conditions"
-                                feedback="You must agree before submitting."
-                            />
-                            <Button type="submit">Submit form</Button>
-
-                        </Form >
+                        <SignUp />
                     </Modal.Body>
                     <Modal.Footer>
                     </Modal.Footer>
--- antisocialfrontend/src/components/SignUp.js
+++ antisocialfrontend/src/components/SignUp.js
@@ -3,34 +3,73 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Button, Col } from 'react-bootstrap/';
 
 class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            validated: false,
+            setValidated: false
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
 
+    }
+
+    handleSubmit(event) {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log("Failed");
+        }
+        this.setState({ validated: true })
+
+    }
     render() {
         return (
-            <Form className="SignUp">
+            <Form className="SignUp" validated={this.validated} onSubmit={this.handleSubmit}>
                 <Form.Row>
-                    <Col>
-                        <Form.Label>First Name</Form.Label>
+                    <Form.Group controlId="validationCustom01">
+                        <Col>
+                            <Form.Label>First Name</Form.Label>
 
-                        <Form.Control placeholder="First name" />
-                    </Col>
-                    <Col>
-                        <Form.Label>Last Name</Form.Label>
+                            <Form.Control
+                                placeholder="First name"
+                                required
+                                type="text"
+                                placeholder="First name"
+                            />
+                            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="validationCustom02">
+                        <Col>
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control
+                                required
+                                type="text"
+                                placeholder="Last name"
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                        <Col>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control required type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                    </Form.Text>
+                        </Col>
+                    </Form.Group>
 
-                        <Form.Control placeholder="Last name" />
-                    </Col>
+
                 </Form.Row>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-              </Form.Text>
-                </Form.Group>
-                <Form.Check
-                    required
-                    label="Agree to terms and conditions"
-                    feedback="You must agree before submitting."
-                />
+                <Col className="d-flex justify-content-between w-100">
+                    <Form.Check
+                        required
+                        label="Agree to terms and conditions"
+                        feedback="You must agree before submitting."
+                    />
+                    <Button className="submitBtn" type="submit" >Sign Up</Button>
+                </Col>
             </Form >
         )
     }
